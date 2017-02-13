32d106171d3c77c3c383617468830826f72b5de3
David Caratelli <devish.c@gmail.com>
2017-02-13 15:23:26 -0600

    updating SNStreamSim

---
+++ uboone/SNStreamSim/Fmwk/CompressionAlgoBase.h
@@ -0,0 +1,105 @@
+/**
+ * \file CompressionAlgoBase.h
+ *
+ * \ingroup DavidAna
+ * 
+ * @author David Caratelli
+ */
+
+/** \addtogroup ClusterRecoUtil
+
+    @{*/
+#ifndef COMPRESSIONALGOBASE_H
+#define COMPRESSIONALGOBASE_H
+
+#include <iostream>
+#include <vector>
+#include <utility> // for pair
+#include "TTree.h"
+
+namespace compress {
+
+  typedef std::vector<short>::const_iterator tick;
+
+  /**
+     \class CMAlgoBase
+     ...
+  */
+  class CompressionAlgoBase {
+    
+  public:
+    
+    /// Default constructor
+    CompressionAlgoBase();
+    
+    /// Default destructor
+    virtual ~CompressionAlgoBase(){}
+
+    /// Function to reset the algorithm instance 
+    virtual void Reset() { _InWF.clear(); _timeRange.clear(); }
+
+    /// Function where compression is performed
+    virtual void ApplyCompression(const std::vector<short> &waveform, const int mode, const unsigned int ch);
+
+    /// Get range of output waveforms
+    virtual const std::vector<std::pair< compress::tick, compress::tick> > GetOutputRanges() { return _timeRange; }
+    
+    /// Setter function for verbosity
+    virtual void SetVerbose(bool doit=true) { _verbose = doit; }
+
+    /// Setter function for debug mode
+    virtual void SetDebug(bool doit=true) { _debug = doit; }
+
+    /// Get Baselines vector
+    virtual const std::vector<double> GetBaselines() { return _baselines; }
+
+    /// Get Variances vector
+    virtual const std::vector<double> GetVariances() { return _variances; }
+
+    /// Get iterator to beginning of input WF
+    virtual const tick GetInputBegin() { return _begin; }
+    /// Get iterator to end of input WF
+    virtual const tick GetInputEnd() { return _end; }
+
+    // set ttree branches
+    virtual void SetTTreeBranches() {}
+
+    // set TTree pointer
+    void setTTree(TTree* tree) { _algo_tree = tree; }
+    
+
+  protected:
+
+    /// Boolean to choose verbose mode.
+    bool _verbose;
+
+    /// Boolean to choose debug mode.
+    bool _debug;
+
+    /// Waveform read in by algorithm...to be scanned and compressed
+    std::vector<short> _InWF;
+
+    /// vector of pairs of iterators marking the beginning/end of the segment to be saved
+    std::vector<std::pair< compress::tick, compress::tick> > _timeRange;
+    /// iterator pointing to current tick
+    tick _thisTick;
+    /// iterator pointing to beginning of input wf
+    tick _begin;
+    /// iterator pointing to end of input wf
+    tick _end;
+
+    /// Vector where to hold the various baselines for the various blocks
+    std::vector<double> _baselines;
+    /// Vector where to hold the variance measured per block
+    std::vector<double> _variances;
+
+    // TTree pointer
+    TTree* _algo_tree;
+
+  };
+
+}
+
+#endif
+/** @} */ // end of doxygen group 
+
