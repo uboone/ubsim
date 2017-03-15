39b2cf0cdc041cea451bb5fa40064c609df13daf
Kevin Wierman <kwierman@gmail.com>
2017-03-15 10:26:35 -0500

    Finished CRTDaqSim build

--- uboone/CRT/CRTDaqSim.hh
+++ uboone/CRT/CRTDaqSim.hh
@@ -8,14 +8,50 @@
 
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Principal/Event.h"
+#include "bernfebdaq-core/Overlays/BernZMQFragment.hh"
 #include "fhiclcpp/ParameterSet.h"
 
 #include <string>
+#include <vector>
+#include <array>
 
-namespace crt{
-  class CRTDaqSim :  public art:: EDProducer{
+namespace art{
+  class Event;
+  class SubRun;
+  class Run;
+}
+
+// TODO: either use include or forward def
+namespace bernfebdaq
+{
+  class BernZMQFragmentMetadata;
+  class BernZMQEvent;
+}
+
+#define N_CRT_FEBS 76
+
+namespace crt
+{
+  class CRTDaqSim :  public art:: EDProducer
+  {
+
+    /// Configuration parameters
+    //  Name of the producer whose products DaqSim will consume
     std::string fProducerName;
+    // The time window in which the boards will be polled in s.
+    uint32_t fPollingTime;
+    // The static time correction diff in s?
+    uint32_t fTimeCorrectionDiff;
+    // The static time offset in s?
+    uint32_t fTimeOffset;
+    // The inter-channel coincidence window time in ns
+    uint32_t fCoincidenceTime;
+
+    /// State machine objects
+    std::array<bernfebdaq::BernZMQFragmentMetadata, N_CRT_FEBS> fMetadata;
+    std::array<std::vector<bernfebdaq::BernZMQEvent>, N_CRT_FEBS> fEvents;
+
+    uint32_t fSeqID;
 
   public:
 
@@ -26,7 +62,13 @@ namespace crt{
     ~CRTDaqSim();
 
     /// art::EDProducer::produce implementation
-    virtual void produce (art::Event&);
+    void produce (art::Event&);
+
+    void reconfigure(fhicl::ParameterSet const&);
+    void beginRun(art::Run &);
+    void beginSubRun(art::SubRun &);
+    void endRun(art::Run &);
+    void endSubRun(art::SubRun &);
 
   };
 }
--- uboone/CRT/CRTDaqSim_module.cc
+++ uboone/CRT/CRTDaqSim_module.cc
@@ -1,6 +1,7 @@
 #include "uboone/CRT/CRTData.hh"
 #include "uboone/CRT/CRTDaqSim.hh"
 
+
 #include "lardata/DetectorInfoServices/DetectorClocksService.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "lardataobj/RawData/RawDigit.h"
@@ -14,8 +15,12 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "uboone/CRT/CRTData.hh"
 #include "artdaq-core/Data/Fragments.hh"
-#include "bernfebdaq-core/Overlays/BernZMQFragment.hh"
+#include "bernfebdaq-core/Overlays/FragmentType.hh"
 
+
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Framework/Principal/Run.h"
 
 #include <vector>
 #include <memory>
@@ -25,10 +30,17 @@
 
 namespace crt{
 
-  CRTDaqSim::CRTDaqSim(const fhicl::ParameterSet& pSet)
+  CRTDaqSim::CRTDaqSim(const fhicl::ParameterSet& pSet):
+    fProducerName(pSet.get<std::string>("InputTag","crt_detsim")) ,
+    fPollingTime(pSet.get<unsigned>("PollingTime",1)),
+    fTimeCorrectionDiff(pSet.get<unsigned>("TimeCorrectionDiff",0)),
+    fTimeOffset(pSet.get<unsigned>("TimeOffset",0)),
+    fCoincidenceTime(pSet.get<unsigned>("CoincidenceTime",0))
   {
+    //TODO: add in either some time jitter (POISSON), or adc noise (Gaussian)
+    //art::ServiceHandle<rndm::NuRandomService> Seeds;
+    //Seeds->createEngine(*this, "", "CRTDaq", pSet, "Seed");
     produces< std::vector<artdaq::Fragment> >();
-
   }
 
   CRTDaqSim::~CRTDaqSim()
@@ -41,32 +53,122 @@ namespace crt{
     std::unique_ptr<std::vector<artdaq::Fragment> > frags(
         new std::vector<artdaq::Fragment>);
 
-    //make the metadata 
-    /*
-      BernZMQFragmentMetadata(uint32_t ts_s, uint32_t ts_ns, 
-        uint32_t te_s, uint32_t te_ns,
-        int t_c, uint64_t t_o,
-        uint32_t r, uint32_t seq,
-        uint64_t fid, uint32_t rid,
-        uint32_t nch, uint32_t nadc)
+    art::Handle<std::vector<crt::CRTData> > crtDataHandle;
+    evt.getByLabel(fProducerName, crtDataHandle);
 
+    for (auto crtDat : *crtDataHandle)
+    {
 
-    bernfebdaq::BernZMQEvent zmqEvent;
-      uint16_t mac5;
-      uint16_t flags;
-      uint16_t lostcpu;
-      uint16_t lostfpga;
-      uint32_t ts0;
-      uint32_t ts1;
-      uint16_t adc[32];
+      //TODO: Check the channel numbering again.
+      unsigned feb_n = crtDat.Channel()/100;
+      unsigned adc_n = crtDat.Channel()%100;
+      bool is_part_of_previous_event = false;
 
-  frags.emplace_back( artdaq::Fragment::FragmentBytes(initial_payload_size,
-                                                      ev_counter(), fragment_id(),
-                                                      fragment_type_, metadata) );
+      for(auto event_it = fEvents[feb_n].begin() ; 
+          event_it != fEvents[feb_n].end(); ++event_it)
+      {
+        if ( crtDat.T0() >=event_it->Time_TS0()-fCoincidenceTime && crtDat.T0() <= event_it->Time_TS0()-fCoincidenceTime  ){
+          is_part_of_previous_event = true;
+          //add the ADC to this one
+          event_it->adc[adc_n] = crtDat.ADC();
 
-    // Performs the hardware coincidence
-  */
+          break;
+        }
+      }
+      if (! is_part_of_previous_event){
+        bernfebdaq::BernZMQEvent zmqEvent;
+        // TODO: Put this into the configuration
+        zmqEvent.mac5 = 5;
+        zmqEvent.flags = 3;
+        zmqEvent.lostcpu=0;
+        zmqEvent.lostfpga=0;
+        zmqEvent.ts0 = crtDat.T0();
+        zmqEvent.ts1 = crtDat.T1();
+        for(int tmp_adc =0; tmp_adc<32; tmp_adc++)
+        {
+          zmqEvent.adc[tmp_adc]=0;
+        }
+        zmqEvent.adc[adc_n]=crtDat.ADC();
+
+        fEvents[feb_n].push_back(zmqEvent);
+      }
+    }
+
+    // Now, Poll, if it's time to poll
+    if(evt.time().value()>=fMetadata[0].time_end_seconds()){
+
+      uint32_t ts_s = fMetadata[0].time_end_seconds();
+      uint32_t ts_ns = 0;
+
+      uint32_t te_s = ts_s + fPollingTime;
+      uint32_t te_ns = 0;
+
+      for(int feb_n=0; feb_n<N_CRT_FEBS; feb_n++){
+
+        auto frag_buf = artdaq::Fragment::FragmentBytes(fMetadata[feb_n].n_events()*sizeof(bernfebdaq::BernZMQEvent),  
+                  fMetadata[feb_n].sequence_number(),feb_n,
+                  bernfebdaq::detail::FragmentType::BernZMQ, fMetadata[feb_n]);
+
+        frags->emplace_back( *frag_buf );
+        std::copy(fEvents[feb_n].begin(),fEvents[feb_n].end(),(bernfebdaq::BernZMQEvent*)(frags->back().dataBegin()));
+
+        //TODO: This might have to be moved to after the evt.put function
+
+        fMetadata[feb_n] = bernfebdaq::BernZMQFragmentMetadata(ts_s, ts_ns, 
+          te_s, te_ns,
+          fTimeCorrectionDiff, fTimeOffset,
+          evt.id().run(), 0,
+          feb_n, 0,
+          32, 32);
+        fEvents[feb_n] = std::vector<bernfebdaq::BernZMQEvent>();
+      }
+    }
     evt.put(std::move(frags));
+  }
+
+  void CRTDaqSim::reconfigure(fhicl::ParameterSet const& pSet)
+  {
+
+  }
+
+  void CRTDaqSim::beginRun(art::Run& run)
+  {
+
+  }
+
+  void CRTDaqSim::beginSubRun(art::SubRun& subrun)
+  {
+    //TODO: Verify that the time conversion here is correct
+    uint32_t ts_s = subrun.beginTime().value();
+    uint32_t ts_ns = 0;
+
+    uint32_t te_s = ts_s + fPollingTime;
+    uint32_t te_ns = 0;
+
+    uint32_t rid=0; //TODO All readers are 0 at the moment
+
+    uint32_t nch=32;
+
+
+    for(int feb_n=0;feb_n<N_CRT_FEBS;feb_n++)
+    {
+      fMetadata[feb_n] = bernfebdaq::BernZMQFragmentMetadata(ts_s, ts_ns, 
+        te_s, te_ns,
+        fTimeCorrectionDiff, fTimeOffset,
+        subrun.id().run(), 0,
+        feb_n, rid,
+        nch, 32);
+      fEvents[feb_n] = std::vector<bernfebdaq::BernZMQEvent>();
+    }
+
+  }
+
+  void CRTDaqSim::endRun(art::Run& run)
+  {
+  }
+
+  void CRTDaqSim::endSubRun(art::SubRun& subrun)
+  {
   }
 
 
