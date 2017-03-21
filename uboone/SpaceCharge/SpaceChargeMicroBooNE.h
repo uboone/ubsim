f580b3fd16274beff1d282d1462196b7ca21e667
Gianluca Petrillo <petrillo@fnal.gov>
2017-03-20 19:35:34 -0500

    Polynomial evaluation replaced from TF1 to hand-coded.
    I would have used GSL if I could figure out how to link the thing.

--- uboone/SpaceCharge/SpaceChargeMicroBooNE.h
+++ uboone/SpaceCharge/SpaceChargeMicroBooNE.h
@@ -23,10 +23,63 @@
 // C/C++ standard libraries
 #include <string>
 #include <vector>
+#include <array>
+#include <algorithm> // std::copy_n()
+
 
 
 namespace spacecharge {
-
+  
+  /// GSL polynomial evaluation wrapper.
+  template <unsigned int N>
+  struct SimplePolynomialBase {
+    static constexpr unsigned int Degree = N; ///< Degree of the polynomial.
+    
+    /// Number of parameters.
+    static constexpr unsigned int NParams = Degree + 1;
+    
+    using Params_t = std::array<double, NParams>; ///< Set of parameters.
+    
+    /// Evaluates the polynomial with specified parameters at point `x`.
+    static double Eval(double x, double const* params);
+    
+    /// Evaluates the polynomial with specified parameters at point `x`.
+    static double Eval(double x, Params_t const& params)
+      { return Eval(x, params.data()); }
+    
+  }; // class SimplePolynomialBase<>
+  
+  
+  /// Implementation of polynomial evaluations mimicking some of TF1 I/F.
+  template <unsigned int N>
+  class SimplePolynomial: public SimplePolynomialBase<N> {
+    using PolyImpl_t = SimplePolynomialBase<N>;
+      public:
+    static constexpr unsigned int Degree = PolyImpl_t::Degree;
+    static constexpr unsigned int NParams = PolyImpl_t::NParams;
+    
+    using Params_t = typename PolyImpl_t::Params_t;
+    
+    /// Default constructor: coefficients are *not initialised*.
+    SimplePolynomial() = default;
+    
+    /// Copies the specified parameters into this object.
+    void SetParameters(double const* params)
+      { std::copy_n(params, NParams, fParams.begin()); }
+    
+    
+    using SimplePolynomialBase<N>::Eval;
+    
+    /// Evaluates the polynomial at the specified point `x`.
+    double Eval(double x) const
+      { return PolyImpl_t::Eval(x, fParams.data()); }
+    
+      protected:
+    Params_t fParams; ///< Parameters of the polynomial.
+    
+  }; // class SimplePolynomial<>
+  
+  
   class SpaceChargeMicroBooNE : public SpaceCharge {
  
     public:
@@ -81,27 +134,27 @@ namespace spacecharge {
       std::vector<TGraph *> g3_z { 7, nullptr };
       std::vector<TGraph *> g4_z { 7, nullptr };
       
-      TF1 *f1_x = new TF1("f1_x","pol6");
-      TF1 *f2_x = new TF1("f2_x","pol6");
-      TF1 *f3_x = new TF1("f3_x","pol6");
-      TF1 *f4_x = new TF1("f4_x","pol6");
-      TF1 *f5_x = new TF1("f5_x","pol6");
-      TF1 *fFinal_x = new TF1("fFinal_x","pol4");
+      typedef SimplePolynomial<6> f1_x_poly_t;
+      typedef SimplePolynomial<6> f2_x_poly_t;
+      typedef SimplePolynomial<6> f3_x_poly_t;
+      typedef SimplePolynomial<6> f4_x_poly_t;
+      typedef SimplePolynomial<6> f5_x_poly_t;
+      typedef SimplePolynomialBase<4> fFinal_x_poly_t;
       
-      TF1 *f1_y = new TF1("f1_y","pol5");
-      TF1 *f2_y = new TF1("f2_y","pol5");
-      TF1 *f3_y = new TF1("f3_y","pol5");
-      TF1 *f4_y = new TF1("f4_y","pol5");
-      TF1 *f5_y = new TF1("f5_y","pol5");
-      TF1 *f6_y = new TF1("f6_y","pol5");
-      TF1 *fFinal_y = new TF1("fFinal_y","pol5");
+      typedef SimplePolynomial<5> f1_y_poly_t;
+      typedef SimplePolynomial<5> f2_y_poly_t;
+      typedef SimplePolynomial<5> f3_y_poly_t;
+      typedef SimplePolynomial<5> f4_y_poly_t;
+      typedef SimplePolynomial<5> f5_y_poly_t;
+      typedef SimplePolynomial<5> f6_y_poly_t;
+      typedef SimplePolynomialBase<5> fFinal_y_poly_t;
       
-      TF1 *f1_z = new TF1("f1_z","pol4");
-      TF1 *f2_z = new TF1("f2_z","pol4");
-      TF1 *f3_z = new TF1("f3_z","pol4");
-      TF1 *f4_z = new TF1("f4_z","pol4");
-      TF1 *fFinal_z = new TF1("fFinal_z","pol3");
-
+      typedef SimplePolynomial<4> f1_z_poly_t;
+      typedef SimplePolynomial<4> f2_z_poly_t;
+      typedef SimplePolynomial<4> f3_z_poly_t;
+      typedef SimplePolynomial<4> f4_z_poly_t;
+      typedef SimplePolynomialBase<3> fFinal_z_poly_t;
+    
       std::vector<TGraph *> g1_Ex { 7, nullptr };
       std::vector<TGraph *> g2_Ex { 7, nullptr };
       std::vector<TGraph *> g3_Ex { 7, nullptr };
@@ -120,26 +173,26 @@ namespace spacecharge {
       std::vector<TGraph *> g3_Ez { 7, nullptr };
       std::vector<TGraph *> g4_Ez { 7, nullptr };
       
-      TF1 *f1_Ex = new TF1("f1_Ex","pol6");
-      TF1 *f2_Ex = new TF1("f2_Ex","pol6");
-      TF1 *f3_Ex = new TF1("f3_Ex","pol6");
-      TF1 *f4_Ex = new TF1("f4_Ex","pol6");
-      TF1 *f5_Ex = new TF1("f5_Ex","pol6");
-      TF1 *fFinal_Ex = new TF1("fFinal_Ex","pol4");
+      typedef SimplePolynomial<6> f1_Ex_poly_t;
+      typedef SimplePolynomial<6> f2_Ex_poly_t;
+      typedef SimplePolynomial<6> f3_Ex_poly_t;
+      typedef SimplePolynomial<6> f4_Ex_poly_t;
+      typedef SimplePolynomial<6> f5_Ex_poly_t;
+      typedef SimplePolynomialBase<4> fFinal_Ex_poly_t;
       
-      TF1 *f1_Ey = new TF1("f1_Ey","pol5");
-      TF1 *f2_Ey = new TF1("f2_Ey","pol5");
-      TF1 *f3_Ey = new TF1("f3_Ey","pol5");
-      TF1 *f4_Ey = new TF1("f4_Ey","pol5");
-      TF1 *f5_Ey = new TF1("f5_Ey","pol5");
-      TF1 *f6_Ey = new TF1("f6_Ey","pol5");
-      TF1 *fFinal_Ey = new TF1("fFinal_Ey","pol5");
+      typedef SimplePolynomial<5> f1_Ey_poly_t;
+      typedef SimplePolynomial<5> f2_Ey_poly_t;
+      typedef SimplePolynomial<5> f3_Ey_poly_t;
+      typedef SimplePolynomial<5> f4_Ey_poly_t;
+      typedef SimplePolynomial<5> f5_Ey_poly_t;
+      typedef SimplePolynomial<5> f6_Ey_poly_t;
+      typedef SimplePolynomialBase<5> fFinal_Ey_poly_t;
       
-      TF1 *f1_Ez = new TF1("f1_Ez","pol4");
-      TF1 *f2_Ez = new TF1("f2_Ez","pol4");
-      TF1 *f3_Ez = new TF1("f3_Ez","pol4");
-      TF1 *f4_Ez = new TF1("f4_Ez","pol4");
-      TF1 *fFinal_Ez = new TF1("fFinal_Ez","pol3");
+      typedef SimplePolynomial<4> f1_Ez_poly_t;
+      typedef SimplePolynomial<4> f2_Ez_poly_t;
+      typedef SimplePolynomial<4> f3_Ez_poly_t;
+      typedef SimplePolynomial<4> f4_Ez_poly_t;
+      typedef SimplePolynomialBase<3> fFinal_Ez_poly_t;
     
     /// Sorts specified graphs.
     void SortGraphs(std::vector<TGraph*> const& graphs);
@@ -149,4 +202,26 @@ namespace spacecharge {
     
   }; // class SpaceChargeMicroBooNE
 } //namespace spacecharge
+
+
+
+//------------------------------------------------------------------------------
+//--- Template implementation
+//---
+template <unsigned int N>
+double spacecharge::SimplePolynomialBase<N>::Eval
+  (double x, double const* params)
+{
+  // gave up using GSL because I cant figure out how to convince CET build to link to it
+  double const* param = params + NParams;
+  double p = *--param;
+  while (param != params) {
+    p *= x;
+    p += *--param;
+  }
+  return p;
+} // spacecharge::SimplePolynomialBase<N>::Eval()
+
+//------------------------------------------------------------------------------
+
 #endif // SPACECHARGE_SPACECHARGEMICROBOONE_H
