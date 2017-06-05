3cc0b0a511e697a8a5878883c49302553f54c381
Usher, Tracy L <usher@slac.stanford.edu>
2017-06-05 10:43:16 -0700

    Remove code to reject ROI’s if large negative average and/or max/min. This was causing some loss of signal. As well, remove commented code from Leon which doesn’t apply now.

--- uboone/CalData/DeconTools/ROIDeconvolution_tool.cc
+++ uboone/CalData/DeconTools/ROIDeconvolution_tool.cc
@@ -153,26 +153,6 @@ void ROIDeconvolution::Deconvolve(IROIFinder::Waveform&             waveform,
         // Fill the buffer and do the deconvolution
         std::copy(waveform.begin()+firstOffset, waveform.begin()+secondOffset, holder.begin());
         
-        // Leon's algorithm for identifying charge collection on the afflicted v plane wires
-//        if (thePlane == 1 && wids[0].Wire > 1180 && wids[0].Wire < 1890)
-//        {
-//            std::vector<float>::iterator maxItr = std::max_element(holder.begin() + roiStart, holder.begin() + roiStop);
-//            std::vector<float>::iterator minItr = std::min_element(maxItr,                    holder.begin() + roiStop);
-//
-//            if (std::distance(maxItr,minItr) <= 30)
-//            {
-//                float maxPulseHeight = *maxItr;
-//                float minPulseHeight = *minItr;
-//
-//                if (maxPulseHeight >= 40. && std::fabs(minPulseHeight/maxPulseHeight) < 0.5)
-//                {
-//                    std::cout << "********> plane: " << thePlane << ", wire: " << wids[0].Wire << ", start: " << firstOffset << ", stop: " << secondOffset << std::endl;
-//                    std::cout << "          max to min distance: " << std::distance(maxItr,minItr) << ", max/min: " << maxPulseHeight << "/" << minPulseHeight << std::endl;
-//                    //                            deconChannel = 6000;
-//                }
-//            }
-//        }
-        
         // Deconvolute the raw signal
         fSignalShaping->Deconvolute(channel,holder);
         
@@ -199,21 +179,9 @@ void ROIDeconvolution::Deconvolve(IROIFinder::Waveform&             waveform,
                 for (size_t iholder = 0; iholder < holder.size(); ++iholder) holder[iholder] *= constant;
             }
         }
-        
-        //wes 23.12.2016 --- sum up the roi, and if it's very negative get rid of it
-        float average_val = std::accumulate(holder.begin(),holder.end(),0.0) / holder.size();
-        float min         = *std::min_element(holder.begin(),holder.end());
-        float max         = *std::max_element(holder.begin(),holder.end());
-        
-        if ((max + average_val) > 1. && (average_val > fMinROIAverageTickThreshold ||  std::abs(min)<std::abs(max)))
-        {
-            // add the range into ROIVec
-            ROIVec.add_range(roi.first, std::move(holder));
-        }
-//        else
-//        {
-//            std::cout << "=======> Rejecting ROI due to average_val, plane: " << thePlane << ", wire: " << wids[0].Wire << ", val: " << average_val << ", min/max " << min << "/" << max << ", firstOffset: " << firstOffset << ", len: " << roiLen << std::endl;
-//        }
+
+        // add the range into ROIVec
+        ROIVec.add_range(roi.first, std::move(holder));
     } // loop over candidate roi's
     
     return;
