fe2d2f5a12ac503b8d7b03fdacf427ea19582de2
Herbert Greenlee <greenlee@fnal.gov>
2017-06-16 09:45:14 -0500

    Relax tolerance on PropTest test.

--- test/RecoObjects/PropTest_module.cc
+++ test/RecoObjects/PropTest_module.cc
@@ -226,9 +226,9 @@ namespace trkf
 	double m1 = mom1[2]*mom2[0] - mom1[0]*mom2[2];
 	double m2 = mom1[0]*mom2[1] - mom1[1]*mom2[0];
 	std::cout << "mom1 x mom2 = " << m0 << ", " << m1 << ", " << m2 << std::endl;
-	assert(std::abs(m0) <= 1.e-8);
-	assert(std::abs(m1) <= 1.e-8);
-	assert(std::abs(m2) <= 1.e-8);
+	assert(std::abs(m0) <= 1.e-6);
+	assert(std::abs(m1) <= 1.e-6);
+	assert(std::abs(m2) <= 1.e-6);
 
 	// Make sure displacement vector is parallel to initial momentum.
 
