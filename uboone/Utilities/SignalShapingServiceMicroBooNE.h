24ed19e910480ab900279e24d9b60abb6d56c1d8
Brandon Eberly <eberly@fnal.gov>
2017-07-17 16:59:10 -0500

    In SetDecon(), retain old FFT size to convolute responses before changing for resampling and kernel calculation

--- uboone/Utilities/SignalShapingServiceMicroBooNE.h
+++ uboone/Utilities/SignalShapingServiceMicroBooNE.h
@@ -248,7 +248,8 @@ namespace util {
 
 
       //Convolution and Deconvolution
-      std::vector<util::SignalShapingContainer> fSignalShapingVec; ///< Stores the convolution and deconvolution kernels    
+      std::vector<util::SignalShapingContainer> fSignalShapingVec; ///< Stores the convolution and deconvolution kernels   
+      unsigned int fConvFFTSize;                                   ///< Stores the FFTSize used for convolution 
       std::vector<int>                          fDeconvPol;        ///< switch for DeconvKernel normalization sign (+ -> max pos ADC, - -> max neg ADC). Entry 0,1,2 = U,V,Y plane settings
 
 
