58a9fa35a51b37d2431f0b4ecb12bac9f7b0dc45
Pawel Guzowski <pawel.guzowski@manchester.ac.uk>
2017-08-17 11:00:40 -0500

    Calculate channel rms with 16/84% quantiles; store event-by-event

--- uboone/DQMTools/DQMChannelNoiseModule_module.cc
+++ uboone/DQMTools/DQMChannelNoiseModule_module.cc
@@ -23,15 +23,13 @@
 #include "lardataobj/RawData/RawDigit.h"
 #include "lardataobj/RawData/raw.h"
 
-#include "TH1D.h"
-#include "TH2D.h"
+#include "TH2F.h"
 
 namespace dqm {
   class DQMChannelNoiseModule;
 }
 
 const uint32_t num_channels = 8256;
-
 
 
 class dqm::DQMChannelNoiseModule : public art::EDAnalyzer {
@@ -57,53 +55,24 @@ public:
 private:
 
   // Declare member data here.
-  std::vector<unsigned short> fADCs[num_channels];
-  unsigned long long fTotalTicks[num_channels];
-  unsigned long long fTotalADC[num_channels];
-  unsigned long long fTotalADCSquared[num_channels];
 
   std::string fDigitModuleLabel;
-  std::vector<uint32_t> fChannelsToHistogram;
-  uint32_t fMaxChannel;
 
-  std::vector<TH1D *> fAdcHists;
-  TH1D *fMeanHist, *fVarHist, *fRMSHist, *fLengthHist, *fPlaneHist;
+  TH2F *fVarHist;
 };
 
 
 dqm::DQMChannelNoiseModule::DQMChannelNoiseModule(fhicl::ParameterSet const & p)
   :
-  EDAnalyzer(p), fMaxChannel(0)  // ,
+  EDAnalyzer(p)  
  // More initializers here.
 { this->reconfigure(p);
-  for(uint32_t i =0; i < num_channels; ++i) {
-    fTotalTicks[i] = fTotalADC[i] = fTotalADCSquared[i] = 0;
-    //fADCs[i].resize(100000);
-  }
 }
 
 void dqm::DQMChannelNoiseModule::analyze(art::Event const & e)
 {
   // Implementation of required member function here.
   //
-  /*
-  if(!(fLengthHist->GetEntries()>0.)) {
-    geo::GeometryCore const* geom = lar::providerFrom<geo::Geometry>();
-    for(uint32_t chan = 0; chan < num_channels; ++chan) {
-      std::vector<geo::WireID> wireIDs = geom->ChannelToWire(chan);
-      if(wireIDs.empty()) {
-        continue;
-      }
-      geo::WireGeo const& wire = geom->Wire(wireIDs.front());
-      double wirelength = wire.HalfL() * 2;
-      fLengthHist->Fill(chan, wirelength);
-      fPlaneHist->Fill(chan, geom->View(wireIDs.front().planeID()));
-    }
-    if(!(fLengthHist->GetEntries()>0.)) {
-      fLengthHist->Fill(0.,0.); // so that GetEntries() >0 on later events, even if there are no wires.
-    }
-  }
-  */
   art::Handle< std::vector<raw::RawDigit> > rawdigit;
   if (! e.getByLabel(fDigitModuleLabel, rawdigit)) {
     std::cout << "WARNING: no label " << fDigitModuleLabel << std::endl;
@@ -113,27 +82,50 @@ void dqm::DQMChannelNoiseModule::analyze(art::Event const & e)
   art::fill_ptr_vector(wires, rawdigit);
 
   for (auto const& wire: wires) {
+    std::map<Short_t, unsigned int> entries;
     std::vector<Short_t> uncompressed(wire->Samples());
     raw::Uncompress(wire->ADCs(), uncompressed, wire->Compression());
-    if(fMaxChannel < wire->Channel()) fMaxChannel=wire->Channel();
-    if(wire->Channel() >= num_channels) {
-      std::cerr << "Wire channel " << wire->Channel() << " > num_channels! " << std::endl;
-      continue;
-    }
     for (size_t j = 0; j < wire->Samples(); ++j){
-      fTotalTicks[wire->Channel()] += 1;
-      fTotalADC[wire->Channel()] += uncompressed[j];
-      fTotalADCSquared[wire->Channel()] += uncompressed[j]*uncompressed[j];
-      //fADCs[wire->Channel()].push_back(uncompressed[j]);
-      //if(fChannelToHistogram >= 0 && wire->Channel() == fChannelToHistogram) {
-      int ii = 0;
-      for(auto i : fChannelsToHistogram) {
-        if(wire->Channel()==i) {
-        fAdcHists[ii]->Fill(uncompressed[j]);
-        }
-        ii++;
+      entries[uncompressed[j]]+=1;
+    }
+    // RMS based on 16 - 84 % quantiles
+    float num_seen = 0.;
+    Short_t low16 = -1;
+    Short_t upp16 = 0x7FFF;
+    for(auto entry : entries) {
+      if(upp16==0x7FFF && num_seen / (float)wire->Samples() >= 0.84) {
+        upp16 = entry.first;
+      }
+      num_seen += entry.second;
+      if(low16 < 0 && num_seen / (float) wire->Samples() >= 0.16) {
+        low16 = entry.first;
       }
     }
+    long long total = 0, totSq = 0;
+    double N=0;
+    for(auto entry : entries) {
+      if(entry.first >= low16 && entry.first < upp16) {
+        long long adc = entry.first;
+        long long nent = entry.second;
+        total += nent * adc;
+        totSq += nent * adc * adc;
+        N += nent;
+      }
+    }
+    float wire_rms = ((N>0) ? ((totSq - total/N*total) / N) : 0.);
+    fVarHist->Fill(wire->Channel(), std::log10(wire_rms+1e-6)); // 1e-6 to offset any log(0) errors
+
+    /*
+    if(!(std::log10(wire_rms+1e-6)>=-6.&&std::log10(wire_rms+1e-6)<=3.)) {
+      for(auto entry : entries) {
+        std::cout << "entries["<<entry.first<<"] = "<<entry.second<<std::endl;
+      }
+      std::cout << "Chan: " << wire->Channel() << std::endl;
+      std::cout << "NS " << num_seen << " " << low16 << " " << upp16 << std::endl;
+      std::cout << "final " << total << " sq: " << totSq << " n:" << N << " rms "<<wire_rms << std::endl ;
+      throw std::exception();
+    }
+    */
   }
 }
 
@@ -142,58 +134,37 @@ void dqm::DQMChannelNoiseModule::beginJob()
   // Implementation of optional member function here.
   art::ServiceHandle<art::TFileService> tfs;
 
-  //fAdcHist = tfs->make<TH2D>("hADC", "adc ", num_channels ,0,num_channels, 65536, 0, 65536);
-  fMeanHist = tfs->make<TH1D>("hADCMean","adc mean", num_channels, 0, num_channels);
-  fVarHist = tfs->make<TH1D>("hADCStdDev","adc std dev", num_channels, 0, num_channels);
-  /*
-  fLengthHist = tfs->make<TH1D>("hLength","wire length",num_channels,0,num_channels);
-  fPlaneHist = tfs->make<TH1D>("hPlane","wire plane",num_channels,0,num_channels);
-  */
-  for(auto i : fChannelsToHistogram) {
-    fAdcHists.push_back(tfs->make<TH1D>(Form("hADC%d",i),Form("adc chan %d",i),65536,0,65536));
+  // cuts will be based on these numbers
+  const int nImportantBins = 7;
+  double important_bins[nImportantBins] = { 0.9, 1., 1.3, 1.9, 5., 8., 11. };
+  double important_bins_log[nImportantBins];
+  for(int i = 0; i < nImportantBins; ++i) important_bins_log[i] = std::log10(important_bins[i]);
+  const int nbins = 40;
+  double binsYlow = -1.5;
+  double binsYup = 3.;
+  std::vector<double> binsY;
+  for(int i = 0; i < nbins+1-nImportantBins; ++i) {
+    binsY.push_back(binsYlow + i * (binsYup - binsYlow) / (double)(nbins-nImportantBins));
   }
+  for(int i = 0; i < nImportantBins; ++i) {
+    binsY.push_back(important_bins_log[i]);
+  }
+  std::sort(binsY.begin(), binsY.end());
+
+  fVarHist = tfs->make<TH2F>("hADCRMS","adc rms;Channel number;Log_{10} RMS", num_channels, 0, num_channels,nbins,binsY.data());
 }
 
 void dqm::DQMChannelNoiseModule::endJob()
 {
   // Implementation of optional member function here.
 
-  /*
-  unsigned long long maxTotalTicks = 0;
-  uint32_t maxChan = -1;
-  for(auto i: fTotalTicks) {
-    if(i.second > maxTotalTicks) {
-      maxTotalTicks = i.second;
-      maxChan = i.first;
-    }
-  }
-  std::cout << "Max channel: " << maxChan << std::endl;
-  */
-  std::cout << "Max Channels no: " << fMaxChannel << std::endl;
-
-  for(uint32_t i = 0; i < num_channels; ++i) {
-    if(fTotalTicks[i]>0) {
-      double N = fTotalTicks[i];
-      double mean = fTotalADC[i]/N;
-      fMeanHist->Fill(i,mean);
-      double var = (fTotalADCSquared[i]-fTotalADC[i]*fTotalADC[i]/N)/N;
-      fVarHist->Fill(i,sqrt(var));
-    }
-    else {
-      fMeanHist->Fill(i,-1.);
-      fVarHist->Fill(i,-1.);
-    }
-  }
-  for(auto h : fAdcHists) {
-    h->GetXaxis()->SetRange(h->FindFirstBinAbove(0),h->FindLastBinAbove(0));
-  }
 }
 
 void dqm::DQMChannelNoiseModule::reconfigure(fhicl::ParameterSet const & p)
 {
   // Implementation of optional member function here.
   fDigitModuleLabel = p.get<std::string>("DigitModuleLabel");
-  fChannelsToHistogram = p.get<std::vector<uint32_t> >("ChannelsToHistogram");
 }
+
 
 DEFINE_ART_MODULE(dqm::DQMChannelNoiseModule)
