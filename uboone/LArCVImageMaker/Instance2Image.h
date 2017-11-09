ee569adfa0c1cbbde8a4c4d4a3508bea65a37a62
Taritree Wongjirad <taritree@mit.edu>
2017-11-09 17:07:49 -0600

    added dl filters and additional supera modules

---
+++ uboone/LArCVImageMaker/Instance2Image.h
@@ -0,0 +1,26 @@
+#ifndef __SUPERA_INSTANCE_LAR2IMAGE_H__
+#define __SUPERA_INSTANCE_LAR2IMAGE_H__
+//#ifndef __CINT__
+//#ifndef __CLING__
+#include "FMWKInterface.h"
+#include "DataFormat/Image2D.h"
+#include "DataFormat/EventChStatus.h"
+#include <map>
+
+namespace supera {
+
+  //
+  // SimChannel => PixelFlowMaps
+  // 
+  std::vector<larcv::Image2D>
+    Instance2Image( const std::vector<larcv::ImageMeta>& meta_v,
+		    const std::map<int,int>& trackid2ancestorid,
+		    const std::vector<supera::LArSimCh_t>& sch_v,
+		    const std::vector<float>& row_compression_factor,
+		    const std::vector<float>& col_compression_factor,
+		    const int time_offset );
+  
+}
+#endif
+//#endif
+//#endif
