6fc1edc1af044e29ea5a9daf11168c4838d13157
Taritree Wongjirad <taritree@mit.edu>
2017-12-05 22:19:46 -0600

    fixed data fcl for larcv. fixed instance image output

--- uboone/LArCVImageMaker/Instance2Image.cxx
+++ uboone/LArCVImageMaker/Instance2Image.cxx
@@ -11,13 +11,14 @@ namespace supera {
   //
   // SimChannel => PixelFlowMaps
   // 
-  std::vector<larcv::Image2D>
-  Instance2Image( const std::vector<larcv::ImageMeta>& meta_v,
-		  const std::map<int,int>& trackid2ancestorid,
-		  const std::vector<supera::LArSimCh_t>& sch_v,
-		  const std::vector<float>& row_compression_factor,
-		  const std::vector<float>& col_compression_factor,			 
-		  const int time_offset ) {
+  void Instance2Image( const std::vector<larcv::ImageMeta>& meta_v,
+		       const std::map<int,int>& trackid2ancestorid,
+		       const std::vector<supera::LArSimCh_t>& sch_v,
+		       const std::vector<float>& row_compression_factor,
+		       const std::vector<float>& col_compression_factor,			 
+		       const int time_offset,
+		       std::vector<larcv::Image2D>& img_out_v,
+		       std::vector<larcv::Image2D>& ancestor_out_v ) {
     
     LARCV_SINFO() << "Instance ID Image ..." << std::endl;
 
@@ -26,27 +27,36 @@ namespace supera {
     // this groups secondaries with their primary parent
     
     // create images we are going to fill
-    std::vector<larcv::Image2D> img_v;     // ADC value per pixel
-    std::vector<larcv::Image2D> energy_v;  // largest energy deposition
+    std::vector<larcv::Image2D> img_v;      // Filling track ID per pixel
+    std::vector<larcv::Image2D> ancestor_v; // Filling ancestor ID per pixel
+    std::vector<larcv::Image2D> energy_v;   // largest energy deposition
     for ( auto const& meta : meta_v ) {
       LARCV_SINFO() << meta.dump();      
       larcv::Image2D img1(meta);
       img1.paint(-1.0);
       img_v.emplace_back( std::move(img1) );
+      
+      larcv::Image2D img2(meta);
+      img2.paint(-1.0);
+      ancestor_v.emplace_back( std::move(img2) );
+
       larcv::Image2D Eimg(meta);
       Eimg.paint(-1.0);
       energy_v.emplace_back( std::move(Eimg) );
     }
-
+    
     // loop over sim channel information
+    //int num_no_ancestor = 0;
+    //std::set<int> noancestorids;
     for (auto const& sch : sch_v) {
       auto ch = sch.Channel();
       auto const& wid   = ::supera::ChannelToWireID(ch);
       auto const& plane = wid.Plane;
 
       
-      auto& imgmap1    = img_v.at(plane);
-      auto& Eimg       = energy_v.at(plane);
+      auto& imgmap1     = img_v.at(plane);
+      auto& ancestormap = ancestor_v.at(plane);
+      auto& Eimg        = energy_v.at(plane);
       
       auto const& meta = imgmap1.meta();
 
@@ -55,7 +65,6 @@ namespace supera {
       if (col < meta.min_x()) continue;
       if (meta.max_x() <= col) continue;
       if (plane != meta.plane()) continue;
-
       // remove offset to get position inside image
       col -= (size_t)(meta.min_x());
       
@@ -77,22 +86,43 @@ namespace supera {
 	  
 	  // for the Y-plane (ipass==0), we store the deposition with the highest energy
 	  if (edep.energy < energy ) continue; // lower deposition than before
-	  if ( trackid2ancestorid.find( edep.trackID )==trackid2ancestorid.end() ) continue;
+	  // if ( trackid2ancestorid.find( edep.trackID )==trackid2ancestorid.end() ) {
+	  //   num_no_ancestor++;
+	  //   noancestorids.insert( edep.trackID );
+	  //   continue;
+	  // }
 	  
 	  energy = edep.energy;
 	  auto it_map = trackid2ancestorid.find( edep.trackID );
-	  ancestorid = it_map->second;
+
+	  if ( it_map!=trackid2ancestorid.end() ) {
+	    // found ID in ID -> ancestor map	  
+	    ancestorid = it_map->second;
+	    ancestormap.set_pixel( row, col, ancestorid );
+	  }
 	  
+	  // regardless if we find ancestor, we store trackID
+	  imgmap1.set_pixel( row, col, edep.trackID ); // raw ID
 	  // we have non-zero energy deposition, valid edep
 	  Eimg.set_pixel( row, col, energy );
-	  imgmap1.set_pixel( row, col, ancestorid );
+	  
 	}
       }
       
     }//end of pass loop
+    // std::cout << "Number of no ancestor pixels: " << num_no_ancestor << std::endl;
+    // std::cout << "Number of no ancestor IDs: " << noancestorids.size() << std::endl;
+    // std::cout << "   ";
+    // for ( auto const& id : noancestorids ) {
+    //   std::cout << id << ", ";
+    // }
+    // std::cout << std::endl;
     
     // make output, compressed images
-    std::vector<larcv::Image2D> img_out_v;
+    //std::vector<larcv::Image2D> img_out_v;
+    //std::vector<larcv::Image2D> ancestor_out_v;
+    img_out_v.clear();
+    ancestor_out_v.clear();
     for ( auto const& img : img_v ) {
       const larcv::ImageMeta& meta = img.meta();
       larcv::ImageMeta meta_out(meta.width(), meta.height(), 
@@ -100,17 +130,23 @@ namespace supera {
 				meta.min_x(), meta.max_y(), 
 				meta.plane() );
       larcv::Image2D img_out( meta_out );
-      img_out.paint(0.0);
+      img_out.paint(-1.0);
       img_out_v.emplace_back( std::move(img_out) );
+      
+      larcv::Image2D ancestor_out( meta_out );
+      ancestor_out.paint(-1.0);
+      ancestor_out_v.emplace_back( std::move(ancestor_out) );
     }
     
     int compressed_pixels_filled = 0;
     for (size_t iidx=0; iidx<img_out_v.size(); iidx++) {
       const larcv::Image2D& img       = img_v[iidx];
+      const larcv::Image2D& ancestor  = ancestor_v[iidx];
       larcv::Image2D& imgout          = img_out_v[iidx];
+      larcv::Image2D& ancestorout     = ancestor_out_v[iidx];
       size_t plane = img.meta().plane();
       const larcv::Image2D& energyimg = energy_v.at( plane );
-
+      
       for (int rout=0; rout<(int)imgout.meta().rows(); rout++) {
 	for (int clout=0; clout<(int)imgout.meta().cols(); clout++) {
 	  // find max pixel to transfer
@@ -123,6 +159,7 @@ namespace supera {
 	      int r = rout *int(row_compression_factor.at(plane)) + dr;
 	      int c = clout*int(col_compression_factor.at(plane)) + dc;
 	      
+	      // no label, we continue
 	      if ( img.pixel(r,c)<0 )
 		continue;
 	      
@@ -139,6 +176,7 @@ namespace supera {
 	  // set the output
 	  if (enmax>0 ) {
 	    imgout.set_pixel( rout, clout, img.pixel( rmax, cmax ) );
+	    ancestorout.set_pixel( rout, clout, ancestor.pixel( rmax, cmax ) );
 	    compressed_pixels_filled++;
 	  }
 	}
@@ -146,11 +184,8 @@ namespace supera {
     }//end of loop over index
     
     
-    std::cout << "compressed pixels filled: " << compressed_pixels_filled << std::endl;
+    //std::cout << "compressed pixels filled: " << compressed_pixels_filled << std::endl;
     
-    //std::cout << "return image" << std::endl;
-
-    return img_out_v;
   }
  
 
--- uboone/LArCVImageMaker/Instance2Image.h
+++ uboone/LArCVImageMaker/Instance2Image.h
@@ -12,13 +12,15 @@ namespace supera {
   //
   // SimChannel => PixelFlowMaps
   // 
-  std::vector<larcv::Image2D>
-    Instance2Image( const std::vector<larcv::ImageMeta>& meta_v,
-		    const std::map<int,int>& trackid2ancestorid,
-		    const std::vector<supera::LArSimCh_t>& sch_v,
-		    const std::vector<float>& row_compression_factor,
-		    const std::vector<float>& col_compression_factor,
-		    const int time_offset );
+  void Instance2Image( const std::vector<larcv::ImageMeta>& meta_v,
+		       const std::map<int,int>& trackid2ancestorid,
+		       const std::vector<supera::LArSimCh_t>& sch_v,
+		       const std::vector<float>& row_compression_factor,
+		       const std::vector<float>& col_compression_factor,
+		       const int time_offset,
+		       std::vector<larcv::Image2D>& img_out_v,
+		       std::vector<larcv::Image2D>& ancestor_out_v );
+
   
 }
 #endif
