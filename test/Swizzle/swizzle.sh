465b886e3a9993b7dcb96bf3c3876617322d71c1
Herbert Greenlee <greenlee@fnal.gov>
2018-03-21 17:34:24 -0500

    Add protection against missing database password file.

--- test/Swizzle/swizzle.sh
+++ test/Swizzle/swizzle.sh
@@ -4,6 +4,21 @@
 
 env > env.txt
 
+# Skip this test of the database password file isn't available on FW_SEARCH_PATH
+
+touch swizzle.skip
+echo $FW_SEARCH_PATH | tr : '\n' | while read dir
+do
+  if [ -f $dir/uboonedb_passwd ]; then
+    rm swizzle.skip
+  fi
+done
+
+if [ -f swizzle.skip ]; then
+  echo "Skipping swizzle integration test because of unavailability of database password file."
+  exit
+fi
+
 # This script runs the standard swizzle fcl file.
 
 input=$UBOONE_EXAMPLE_DATA_DIR/ubdaq/PhysicsRun-2018_2_26_13_49_57-0015224-00007.ubdaq
