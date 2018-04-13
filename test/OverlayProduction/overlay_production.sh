80b0641be1669ed561b40dcc6776f13d1b27d00c
Herbert Greenlee <greenlee@fnal.gov>
2018-04-13 14:31:15 -0500

    Enable overlay integration test.

--- test/OverlayProduction/overlay_production.sh
+++ test/OverlayProduction/overlay_production.sh
@@ -6,15 +6,15 @@ env > env.txt
 
 # Exit if stash cache isn't mounted.
 
-UBOONE_EXAMPLE_DATA_DIR=/cvmfs/uboone.osgstorage.org/stash/uboone_example_overlay
+UBOONE_EXAMPLE_DATA_DIR=/cvmfs/uboone.osgstorage.org/stash/uboone_example_data
 if [ ! -d $UBOONE_EXAMPLE_DATA_DIR ]; then
   echo "Quittig because stash cache isn't available."
   exit
 fi
 
-# This script runs the full mc+reco chain using standard released fcl files.
+# This script runs the full overlay chain using standard released fcl files.
 
-input=$UBOONE_EXAMPLE_DATA_DIR/pnfs/uboone/data/uboone/raw/swizzle_trigger_streams/mergeext_unbiased/prod_v04_26_04_05/00/00/83/07/PhysicsRun-2016_10_15_0_6_5-0008307-00029_20161015T141511_ext_unbiased_20161020T160531_merged.root
+input=$UBOONE_EXAMPLE_DATA_DIR/swizzled/PhysicsRun-2016_3_14_9_22_21-0005432-00021_20160322T065603_ext_bnb_20160323T041757_merged.root
 for fcl in standard_overlay_gen_driver.fcl standard_g4_uboone.fcl standard_detsim_nonoise_overlay_uboone.fcl standard_overlay_uboone.fcl reco_uboone_mcc8_driver_overlay_stage1a.fcl reco_uboone_mcc8_driver_overlay_stage1b.fcl reco_uboone_mcc8_driver_overlay_stage1c.fcl reco_uboone_mcc8_driver_overlay_stage2.fcl
 do
   output=`basename $fcl .fcl`.root
