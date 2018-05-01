c1626f69b225ff2942ec5bf0a204ff89231716a9
Varuna Meddage <vmeddage@phys.ksu.edu>
2018-05-01 11:38:32 -0500

    Fix the bug

--- uboone/Calibrations/XYZcorrection_module.cc
+++ uboone/Calibrations/XYZcorrection_module.cc
@@ -32,10 +32,12 @@
 #include "lardataobj/RecoBase/EndPoint2D.h"
 #include "lardataobj/RecoBase/Vertex.h"
 #include "lardataobj/RecoBase/OpFlash.h"
+#include "lardataobj/RecoBase/PFParticle.h"
 #include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
 #include "larreco/RecoAlg/TrackMomentumCalculator.h"
 #include "lardataobj/AnalysisBase/CosmicTag.h"
 #include "lardataobj/AnalysisBase/FlashMatch.h"
+#include "lardataobj/AnalysisBase/T0.h"
 
 #include "TFile.h"
 #include "TTree.h"
@@ -87,6 +89,7 @@ public:
     
 private:
     TTree* fEventTree;
+    TTree* fACTree;
     Int_t    run;                  
     Int_t    subrun;               
     Int_t    event;
@@ -108,6 +111,25 @@ private:
     Float_t  trkhitx[kMaxTracks][3][3000];
     Float_t  trkhity[kMaxTracks][3][3000];
     Float_t  trkhitz[kMaxTracks][3][3000];
+    
+    Int_t    AC_trks;
+    Float_t  AC_trklen[kMaxTracks];
+    Int_t    AC_TrkID[kMaxTracks];
+    Float_t  AC_xprojectedlen[kMaxTracks];
+    Float_t  AC_trackthetaxz[kMaxTracks];
+    Float_t  AC_trackthetayz[kMaxTracks];
+    Float_t  AC_start[kMaxTracks][3];
+    Float_t  AC_end[kMaxTracks][3];
+    Float_t  AC_trkstartcosxyz[kMaxTracks][3];
+    Float_t  AC_trkendcosxyz[kMaxTracks][3];
+    Int_t    AC_ntrkhits[kMaxTracks][3];
+    Float_t  AC_trkdqdx[kMaxTracks][3][3000];
+    Float_t  AC_trkdedx[kMaxTracks][3][3000];
+    Float_t  AC_trkresrange[kMaxTracks][3][3000];
+    Float_t  AC_trkhitx[kMaxTracks][3][3000];
+    Float_t  AC_trkhity[kMaxTracks][3][3000];
+    Float_t  AC_trkhitz[kMaxTracks][3][3000];
+    
     std::string fDigitModuleLabel;
     std::string fHitsModuleLabel;
     std::string fLArG4ModuleLabel;
@@ -182,6 +204,31 @@ void XYZcorrection::beginJob(){
   fEventTree->Branch("trkhitx",trkhitx,"trkhitx[cross_trks][3][3000]/F");
   fEventTree->Branch("trkhity",trkhity,"trkhity[cross_trks][3][3000]/F");
   fEventTree->Branch("trkhitz",trkhitz,"trkhitz[cross_trks][3][3000]/F");
+  
+  fACTree = tfs->make<TTree>("ACpierce", "Event Tree from AC pierce");
+  fACTree->Branch("event", &event,"event/I");
+  fACTree->Branch("evttime",&evttime,"evttime/D");
+  fACTree->Branch("run", &run,"run/I");
+  fACTree->Branch("subrun", &subrun,"surbrun/I");
+  fACTree->Branch("year_month_date", &year_month_date,"year_month_date/I");
+  fACTree->Branch("hour_min_sec", &hour_min_sec,"hour_min_sec/I");
+  fACTree->Branch("AC_trks",&AC_trks,"AC_trks/I");
+  fACTree->Branch("AC_trklen",&AC_trklen,"AC_trklen[AC_trks]/F");
+  fACTree->Branch("AC_TrkID",&AC_TrkID,"AC_TrkID[AC_trks]/I");
+  fACTree->Branch("AC_xprojectedlen",AC_xprojectedlen,"AC_xprojectedlen[AC_trks]/F");
+  fACTree->Branch("AC_trackthetaxz",AC_trackthetaxz,"AC_trackthetaxz[AC_trks]/F");
+  fACTree->Branch("AC_trackthetayz",AC_trackthetayz,"AC_trackthetayz[AC_trks]/F");
+  fACTree->Branch("AC_start",AC_start,"AC_start[AC_trks][3]/F");
+  fACTree->Branch("AC_end",AC_end,"AC_end[AC_trks][3]/F");
+  fACTree->Branch("AC_trkstartcosxyz",AC_trkstartcosxyz,"AC_trkstartcosxyz[AC_trks][3]/F");
+  fACTree->Branch("AC_trkendcosxyz",AC_trkendcosxyz,"AC_trkendcosxyz[AC_trks][3]/F");
+  fACTree->Branch("AC_ntrkhits",AC_ntrkhits,"AC_ntrkhits[AC_trks][3]/I");
+  fACTree->Branch("AC_trkdqdx",AC_trkdqdx,"AC_trkdqdx[AC_trks][3][3000]/F");
+  fACTree->Branch("AC_trkdedx",AC_trkdedx,"AC_trkdedx[AC_trks][3][3000]/F");
+  fACTree->Branch("AC_trkresrange",AC_trkresrange,"AC_trkresrange[AC_trks][3][3000]/F");
+  fACTree->Branch("AC_trkhitx",AC_trkhitx,"AC_trkhitx[AC_trks][3][3000]/F");
+  fACTree->Branch("AC_trkhity",AC_trkhity,"AC_trkhity[AC_trks][3][3000]/F");
+  fACTree->Branch("AC_trkhitz",AC_trkhitz,"AC_trkhitz[AC_trks][3][3000]/F");
 }
 
 //========================================================================
@@ -203,11 +250,21 @@ void XYZcorrection::analyze( const art::Event& evt){
      reset();  
      
      art::Handle< std::vector<recob::Track> > trackListHandle;
+     art::Handle< std::vector<recob::Track> > trackListHandle_2;
+     art::Handle< std::vector<recob::PFParticle> > PFPListHandle;
+     
      std::vector<art::Ptr<recob::Track> > tracklist;
-  
+     std::vector<art::Ptr<recob::Track> > tracklist_2;
+     std::vector<art::Ptr<recob::PFParticle> > pfplist;
+     
      if(evt.getByLabel(fTrackModuleLabel,trackListHandle)) art::fill_ptr_vector(tracklist, trackListHandle);
-  
+     if(evt.getByLabel("pandoraCosmic",trackListHandle_2)) art::fill_ptr_vector(tracklist_2, trackListHandle_2);
+     if(evt.getByLabel("pandoraCosmic",PFPListHandle)) art::fill_ptr_vector(pfplist, PFPListHandle);
+     
      art::FindManyP<anab::Calorimetry> fmcal(trackListHandle, evt, fCalorimetryModuleLabel);
+     art::FindManyP<anab::T0> trk_t0_assn_v(trackListHandle_2, evt, "pandoraCosmicT0Reco");
+     art::FindManyP<recob::PFParticle> pfp_trk_assn(trackListHandle_2, evt, "pandoraCosmic");
+     art::FindManyP<recob::Track> pft_trk_2_assn(PFPListHandle, evt, "pandoraCosmicKalmanTrack");
      
      run = evt.run();
      subrun = evt.subRun();
@@ -235,7 +292,6 @@ void XYZcorrection::analyze( const art::Event& evt){
      for(size_t i=0; i<NTracks;++i){
          art::Ptr<recob::Track> ptrack(trackListHandle, i);
 	 std::vector<art::Ptr<anab::Calorimetry>> calos=fmcal.at(i);
-
 	 const recob::Track& track = *ptrack;
 	 TVector3 pos, dir_start, dir_end, end;
 	 pos = track.Vertex();
@@ -326,7 +382,66 @@ void XYZcorrection::analyze( const art::Event& evt){
 	     } // loop over ical 2 nd time...
           } // crossing trks...
       } // loop over trks...
-   fEventTree->Fill();
+     
+     fEventTree->Fill();
+   
+     AC_trks=0;
+     size_t N2Tracks = tracklist_2.size();
+     auto const* _detp = lar::providerFrom<detinfo::DetectorPropertiesService>();
+     double efield = _detp -> Efield();
+     double temp   = _detp -> Temperature();
+     double fDriftVelocity = _detp -> DriftVelocity(efield,temp);
+     for(size_t i=0; i<N2Tracks;++i){
+	 std::vector<art::Ptr<anab::T0>> t0s=trk_t0_assn_v.at(i);
+	 if(!t0s.size()) continue;
+	 std::vector<art::Ptr<recob::PFParticle>> pfps=pfp_trk_assn.at(i);
+	 if(!pfps.size()) continue;
+	 std::vector<art::Ptr<recob::Track>> trks=pft_trk_2_assn.at(pfps[0].key());
+	 if(!trks.size()) continue;
+	 std::vector<art::Ptr<anab::Calorimetry>> calos=fmcal.at(trks[0].key());
+	 auto t0 = t0s.at(0);
+	 double t_zero=t0->Time();
+	 AC_trks++;
+	 //art::Ptr<recob::Track> ptrack(trackListHandle,i);
+	 art::Ptr<recob::Track> ptrack = trks[0];
+	 const recob::Track& track = *ptrack;
+	 TVector3 pos, dir_start, dir_end, end;
+	 pos = track.Vertex();
+     	 dir_start = track.VertexDirection();
+     	 dir_end   = track.EndDirection();
+     	 end = track.End();
+	 double theta_xz = std::atan2(dir_start.X(), dir_start.Z());
+         double theta_yz = std::atan2(dir_start.Y(), dir_start.Z());
+     	 float X = std::abs(pos.X()-end.X());
+	 AC_trklen[AC_trks-1]=track.Length();
+	 AC_TrkID[AC_trks-1]=track.ID();
+	 AC_xprojectedlen[AC_trks-1]=X;
+	 AC_trackthetaxz[AC_trks-1]=theta_xz;
+	 AC_trackthetayz[AC_trks-1]=theta_yz;
+	 AC_start[AC_trks-1][0]=pos.X(); AC_start[AC_trks-1][1]=pos.Y(); AC_start[AC_trks-1][2]=pos.Z();
+	 AC_end[AC_trks-1][0]=end.X(); AC_end[AC_trks-1][1]=end.Y(); AC_end[AC_trks-1][2]=end.Z();
+	 AC_trkstartcosxyz[AC_trks-1][0]=dir_start.X(); AC_trkstartcosxyz[AC_trks-1][1]=dir_start.Y(); AC_trkstartcosxyz[AC_trks-1][2]=dir_start.Z();
+	 AC_trkendcosxyz[AC_trks-1][0]=dir_end.X(); AC_trkendcosxyz[AC_trks-1][1]=dir_end.Y(); AC_trkendcosxyz[AC_trks-1][2]=dir_end.Z();
+	 for(size_t ical = 0; ical<calos.size(); ++ical){
+	     if(!calos[ical]) continue;
+	     if(!calos[ical]->PlaneID().isValid) continue;
+	     int planenum = calos[ical]->PlaneID().Plane;
+	     if(planenum<0||planenum>2) continue;
+	     const size_t NHits = calos[ical] -> dEdx().size();
+             AC_ntrkhits[AC_trks-1][planenum]=int(NHits);
+	     for(size_t iHit = 0; iHit < NHits; ++iHit){
+	         const auto& TrkPos=(calos[ical] -> XYZ())[iHit];
+		 AC_trkdqdx[AC_trks-1][planenum][iHit]=(calos[ical] -> dQdx())[iHit];
+		 AC_trkdedx[AC_trks-1][planenum][iHit]=(calos[ical] -> dEdx())[iHit];
+		 AC_trkresrange[AC_trks-1][planenum][iHit]=(calos[ical]->ResidualRange())[iHit];
+		 AC_trkhitx[AC_trks-1][planenum][iHit]=TrkPos.X()-(t_zero*fDriftVelocity);
+		 AC_trkhity[AC_trks-1][planenum][iHit]=TrkPos.Y();
+		 AC_trkhitz[AC_trks-1][planenum][iHit]=TrkPos.Z();
+	     }
+	 }
+	 
+     } // loop over pandoracosmic trks
+     fACTree->Fill();
 } // end of analyze function
 	   
  /////////////////// Defintion of reset function ///////////
@@ -339,6 +454,7 @@ void XYZcorrection::reset(){
      all_trks = -9999;
      year_month_date=-9999;
      hour_min_sec=-9999;
+     AC_trks=-9999;
      for(int i=0; i<kMaxTracks; i++){
          trackthetaxz[i]=-9999;
 	 trackthetayz[i]=-9999;
@@ -353,14 +469,40 @@ void XYZcorrection::reset(){
 	 ntrkhits[i][0] = -9999;
 	 ntrkhits[i][1] = -9999;
 	 ntrkhits[i][2] = -9999;
+	 AC_trklen[i]=-9999;
+	 AC_TrkID[i]=-9999;
+	 AC_xprojectedlen[i]=-9999;
+	 AC_trackthetaxz[i]=-9999;
+	 AC_trackthetayz[i]=-9999;
+	 AC_start[i][0]=-9999;
+	 AC_start[i][1]=-9999;
+	 AC_start[i][2]=-9999;
+	 AC_end[i][0]=-9999;
+	 AC_end[i][1]=-9999;
+	 AC_end[i][2]=-9999;
+	 AC_trkstartcosxyz[i][0]=-9999;
+	 AC_trkstartcosxyz[i][1]=-9999;
+	 AC_trkstartcosxyz[i][2]=-9999;
+	 AC_trkendcosxyz[i][0]=-9999;
+	 AC_trkendcosxyz[i][1]=-9999;
+	 AC_trkendcosxyz[i][2]=-9999;
+	 AC_ntrkhits[i][0]=-9999;
+	 AC_ntrkhits[i][1]=-9999;
+	 AC_ntrkhits[i][2]=-9999;
 	 for(int j=0; j<3; j++){
 	      for(int k=0; k<3000; k++){
-	           trkdqdx[i][j][k]=-9999;
-		   trkdedx[i][j][k]=-9999;
-		   trkresrange[i][j][k]=-9999;
-		   trkhitx[i][j][k]=-9999;
-		   trkhity[i][j][k]=-9999;
-		   trkhitz[i][j][k]=-9999;
+	          trkdqdx[i][j][k]=-9999;
+		  trkdedx[i][j][k]=-9999;
+		  trkresrange[i][j][k]=-9999;
+		  trkhitx[i][j][k]=-9999;
+		  trkhity[i][j][k]=-9999;
+		  trkhitz[i][j][k]=-9999;
+		  AC_trkdqdx[i][j][k]=-9999;
+		  AC_trkdedx[i][j][k]=-9999;
+		  AC_trkresrange[i][j][k]=-9999;
+		  AC_trkhitx[i][j][k]=-9999;
+		  AC_trkhity[i][j][k]=-9999;
+		  AC_trkhitz[i][j][k]=-9999;
 	      }
 	 }
      }
