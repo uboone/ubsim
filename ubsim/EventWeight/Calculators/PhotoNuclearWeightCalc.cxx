d07261ed308159c3d3bc67ae6b2bcbed5a8d54be
yeonjaej <yj2429@columbia.edu>
2019-12-12 15:53:16 -0600

    commit before tidy up

--- ubsim/EventWeight/Calculators/PhotoNuclearWeightCalc.cxx
+++ ubsim/EventWeight/Calculators/PhotoNuclearWeightCalc.cxx
@@ -13,6 +13,7 @@
 #include "TDirectory.h"
 #include "TFile.h"
 #include "TH1D.h"
+#include "TRandom3.h"
 #include "Geant4/G4LossTableManager.hh"
 #include "Geant4/G4ParticleTable.hh"
 #include "Geant4/G4ParticleDefinition.hh"
@@ -94,6 +95,7 @@ private:
   std::map<int, ParticleDef> fParticles;  //!< Particles to reweight
   unsigned fNsims;  //!< Number of multisims
   float fXSUncertainty;  //!< Flat cross section uncertainty
+  TRandom3 *r3=new TRandom3();
 
   DECLARE_WEIGHTCALC(PhotoNuclearWeightCalc)
 };
@@ -109,7 +111,7 @@ void PhotoNuclearWeightCalc::Configure(fhicl::ParameterSet const& p,
   std::vector<std::string> pars = pset.get< std::vector<std::string> >("parameter_list");//"gamma"	
   std::vector<float> sigmas = pset.get<std::vector<float> >("parameter_sigma");	
   std::string mode = pset.get<std::string>("mode");//"multisim"
-  fXSUncertainty = pset.get<float>("xs_uncertainty", 0.3);
+  fXSUncertainty = pset.get<float>("xs_uncertainty", 1.0);
   std::string probFileName = pset.get<std::string>("ProbFileName", "photonuprob.root");//systematics/reint/interaction_probabilities.root");//what is this file?
   fNsims = pset.get<int> ("number_of_multisims", 0);
 
@@ -166,6 +168,8 @@ PhotoNuclearWeightCalc::GetWeight(art::Event& e) {
   const art::FindManyP<simb::MCParticle> truthParticles(truthHandle, e, fMCParticleProducer);
   assert(truthParticles.isValid());
 
+  std::cout << "Event Number : " << e.id() << std::endl;
+
   // Initialize the vector of event weights
   std::vector<std::vector<double> > weight(truthHandle->size());
 
@@ -179,15 +183,45 @@ PhotoNuclearWeightCalc::GetWeight(art::Event& e) {
     auto const& mcparticles = truthParticles.at(itruth);
 
     int photon_count = 0;
+    int primary_photon_count = 0;
+    std::vector<int> pi0_trackIDs;
 
+    // collecting pi0s in mcparticles
     for (size_t i=0; i<mcparticles.size(); i++) {
       const simb::MCParticle& p = *mcparticles.at(i);
       int pdg = p.PdgCode();
+      if (pdg ==111){
+	std::cout <<"pi0 :: EndProcess : " << p.EndProcess() << " , Statuscode : " << p.StatusCode() << " , TrackID : " << p.TrackId() << " , P : " << p.P() << std::endl;
+	pi0_trackIDs.push_back(p.TrackId());
+      }
+    }
+    std::cout << "size of pi0 set : " << pi0_trackIDs.size() << std::endl;
 
-      if (pdg == 22) photon_count++;
+    for (size_t i=0; i<mcparticles.size(); i++) {
+
+      const simb::MCParticle& p = *mcparticles.at(i);
+      int pdg = p.PdgCode();
+
+      bool is_from_pi0=false;
+
+      if (pdg == 22){
+	//	std::cout <<itruth<<"-th truth, "<< photon_count<<"-th photon :: EndProcess : " << p.EndProcess() << " , Mother: " << p.Mother() << " , Statuscode : " << p.StatusCode() << " , TrackID : " << p.TrackId() << " , P : " << p.P() << std::endl;
+	photon_count++;
+
+	if (std::find(pi0_trackIDs.begin(), pi0_trackIDs.end(),p.Mother())!=pi0_trackIDs.end()){
+	  is_from_pi0 = true;
+	  primary_photon_count++;
+	  std::cout <<itruth<<"-th truth, "<< primary_photon_count<<"-th primary photon :: EndProcess : " << p.EndProcess() << " , Mother: " << p.Mother() << " , Statuscode : " << p.StatusCode() << " , TrackID : " << p.TrackId() << " , P : " << p.P() << " , primary? :" <<is_from_pi0<<std::endl;
+	}
+	
+      }
+      
+      if (!is_from_pi0) continue;
 
       double ke = p.E() - p.Mass();
       std::string endProc = p.EndProcess();
+
+
 
       bool photonu_absorbed = (endProc.find("Photonuclear") != std::string::npos);
       //      bool interaced = (endProc.find("Inelastic") != std::string::npos);
@@ -201,36 +235,51 @@ PhotoNuclearWeightCalc::GetWeight(art::Event& e) {
         for (size_t j=0; j<weight[0].size(); j++) {
           // Integrate a modified cross section to find a survival probability
           float sprob = 1.0;
+
+	  /*
           for (int k=1; k<kebin+1; k++) {
             float wbin = 1.0 + fXSUncertainty * def.sigmas[j];
             float xs = wbin * def.xs->GetBinContent(k);
             sprob *= exp(-1.0 * xs);
           }
+	  */
+
+	  //just use prob?
+	  float wbin = 1.0 + fXSUncertainty * def.sigmas[j];                                                                             
+	  if (wbin<0.) {
+	    wbin = 0.;
+	    std::cout << "setting wbin to be non-negative, wbin is set to :"<< wbin << std::endl; 
+	  }                   
+	  //float xs = wbin * def.xs->GetBinContent(kebin);     
+	  sprob = 1- wbin*def.pint->GetBinContent(kebin); //exp(-1.0 * xs); 
 
           float w;
           if (photonu_absorbed) {
             w = (1.0 - sprob) / def.pint->GetBinContent(kebin);
+	    std::cout << "photonu! , nominal sprob : " << 1-def.pint->GetBinContent(kebin) << " , sprob= "<<sprob<<", when r= "<< def.sigmas[j] << " , weight  : "<< w <<std::endl; 
           }
           else {
             w = sprob / (1.0 - def.pint->GetBinContent(kebin));
+	    std::cout << "no photonu!, nominal sprob : " << 1-def.pint->GetBinContent(kebin) << " , sprob= "<<sprob<<", when r= "<< def.sigmas[j]<< " , weight: " << w << std::endl;
           }
+	  
 
           // Total weight is the product of track weights in the event
 	  // Maybe we should just do it  for primary photons
           weight[itruth][j] *= std::max((float)0.0, w);
 
-	  std::cout << "itruth : " << itruth << " , j-th univ : " << j << " , weight : " << std::max((float)0.0, w) << std::endl;
+	  //std::cout << "itruth : " << itruth << " , j-th univ : " << j << " , weight : " << std::max((float)0.0, w) << std::endl;
 
         }
       }
     }
 
-    std::cout << "photon count : " << photon_count << std::endl;
+    std::cout << "photon count : " << photon_count << " , primary photon count : " << primary_photon_count<<std::endl;
     
   }
   for(size_t idx=0; idx<weight.size(); idx++){
     for (size_t jdx=0; jdx<weight[0].size(); jdx++){
-      std::cout << "i: " <<idx <<" , j: "<<jdx<<"evt_weight : " << weight[idx][jdx] << std::endl;
+      std::cout << idx << "-th truth , " <<jdx <<"-th univ , "<<jdx<<" evt_weight : " << weight[idx][jdx] << std::endl;
     }
   } 
   return weight;
