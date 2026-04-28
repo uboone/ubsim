aa40c226d4c78c3522580a5db018b5e62a712995
Herbert Greenlee <greenlee@fnal.gov>
2026-04-28 12:57:29 -0500

    Fix one definition rule violation.

---
+++ ubsim/EventWeight/Calculators/G4RWManagerService.cc
@@ -0,0 +1,18 @@
+#include "G4RWManagerService.h"
+
+evwgh::G4RWManagerService::G4RWManagerService(
+    fhicl::ParameterSet const& pset) :
+    fMaterial(pset.get<fhicl::ParameterSet>("Material")),
+    fRWManager(new G4ReweightManager({fMaterial})) {}
+
+evwgh::G4RWManagerService::G4RWManagerService(
+    fhicl::ParameterSet const& pset,
+    art::ActivityRegistry&) : G4RWManagerService(pset) {}
+
+G4ReweightManager * evwgh::G4RWManagerService::GetManager() const {
+  return fRWManager;
+}
+
+const fhicl::ParameterSet & evwgh::G4RWManagerService::GetMaterial() const {
+  return fMaterial;
+}
